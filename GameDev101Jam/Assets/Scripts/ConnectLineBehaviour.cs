bf38feed6d4ccf8d8c47da6be5f345f25fd68a02
af <af@pleco.io>
2020-03-15 00:19:44 +0100

    added a simple 3d map system

---
+++ GameDev101Jam/Assets/Scripts/ConnectLineBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectLineBehaviour : MonoBehaviour
+{
+    public Transform PointATransform = default;
+    public Transform PointBTransform = default;
+
+    private LineRenderer _lineRenderer = default;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector3[] points = new Vector3[2];
+        points[0] = PointATransform.position;
+        points[1] = PointBTransform.position;
+        _lineRenderer.SetPositions(points);
+    }
+}
---
+++ GameDev101Jam/Assets/Scripts/RotateBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        float randomOffset = Random.Range(1f, 5f);
+        transform.localRotation *= Quaternion.Euler(randomOffset, randomOffset, randomOffset);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localRotation *= Quaternion.Euler(1f, 0f, 1f);
+    }
+}
